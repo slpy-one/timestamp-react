9ec7dab9d5bf0c791e994ec487969fe2a3bbb664
Suphakit P. <me@suphakit.net>
2025-02-13 09:59:19 +0700

    feat<component>: created footer

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,13 @@
+export const Footer = () => {
+  return (
+    <div className="w-full absolute bottom-0 left-0 p-4 text-right">
+      made with 🤍 by{" "}
+      <a
+        href="https://slpy.one"
+        className="underline hover:no-underline hover:opacity-60"
+      >
+        slpy one team
+      </a>
+    </div>
+  );
+};
