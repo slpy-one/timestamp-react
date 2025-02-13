2820d3bde1c0014a003421afa83691c76e46ecef
Suphakit P. <me@suphakit.net>
2025-02-13 09:55:18 +0700

    feat<component>: created navbar

---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,22 @@
+export const NavBar = () => {
+  return (
+    <div className="w-full flex flex-row absolute top-0 left-0 bg-gradient-to-b from-[#000] to-[transparent]">
+      <div className="w-full flex justify-start border-collapse gap-[.5rem]">
+        <a
+          className="p-4 hover:opacity-60 hover:bg-overlay border-r-[1px] border-solid border-text"
+          href="/"
+        >
+          /Home
+        </a>
+      </div>
+      <div className="w-full flex justify-end border-collapse gap-[.5rem]">
+        <a
+          className="p-4 hover:opacity-60 hover:bg-overlay border-l-[1px] border-solid border-text"
+          href="/about"
+        >
+          /About
+        </a>
+      </div>
+    </div>
+  );
+};
