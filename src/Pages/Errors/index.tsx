cd42ddb034ea99e416c1e4c0c673681227706622
Suphakit P. <me@suphakit.net>
2025-02-13 10:01:15 +0700

    feat<page>: added error page

---
+++ src/Pages/Errors/index.tsx
@@ -0,0 +1,11 @@
+export const ErrorPage = () => {
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <img
+        src="https://http.cat/404"
+        alt="cat"
+        className="w-[50%] h-auto aspect-auto"
+      />
+    </div>
+  );
+};
